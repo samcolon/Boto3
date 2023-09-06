b4e7496bd0ca87a8481569f16fd18cde94284b5c
samcolon <116681627+samcolon@users.noreply.github.com>
2023-09-06 16:24:09 -0400

    updated upload_s3.py

--- upload_s3.py
+++ upload_s3.py
@@ -1,11 +1,11 @@
 import boto3
 
-session = boto3.Session(profile_name='leveluptech')
+session = boto3.Session(profile_name='enter_user_profile_here')
 
 s3 = session.client('s3')
 
 with open("this_text.txt", 'rb') as f:
-    s3.put_object(Bucket="levelup0923123456", Key='this_text.txt', Body=f, ContentType="text/plain")
+    s3.put_object(Bucket="enter_bucket_name", Key='this_text.txt', Body=f, ContentType="text/plain")
     
 ###This is the command for adding a file path###
 
